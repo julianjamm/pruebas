508cf70f6bdfe8ce421031ded5026e16179ab4c9
julianjamm <julianjamm321123@gmail.com>
2025-08-26 17:58:06 -0500

    Add files via upload

---
+++ segmento_7_con_while.py
@@ -0,0 +1,12 @@
+a = [1, 0, 1, 1, 0, 1, 1, 1, 1, 1]
+b = [1, 1, 1, 1, 1, 1, 0, 0, 1, 1]
+c = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+d = [1, 0, 0, 1, 0, 1, 1, 0, 1, 0]
+e = [1, 0, 1, 0, 0, 0, 1, 0, 1, 0]
+f = [1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+g = [0, 0, 1, 1, 1, 1, 1, 1, 1, 0]
+
+segmento = 0
+while segmento < 10:
+    print(f"Número {segmento} = A={a[segmento]} B={b[segmento]} C={c[segmento]} D={d[segmento]} E={e[segmento]} F={f[segmento]} G={g[segmento]}")
+    segmento += 1
---
+++ siete_segmentos.py
@@ -0,0 +1,30 @@
+a = 1
+b = 1
+c = 1
+d = 1
+e = 1
+f = 0
+g = 1
+
+if a == 1 and b == 1 and c == 1 and d == 1 and e == 1 and f == 1 and g == 0:
+    print("este valor es cero")
+elif a == 0 and b == 1 and c == 1 and d == 0 and e == 0 and f == 0 and g == 0:
+    print("este valor es uno")
+elif a == 1 and b == 1 and c == 0 and d == 1 and e == 1 and f == 0 and g == 1:
+    print("este valor es dos")
+elif a == 1 and b == 1 and c == 1 and d == 1 and e == 0 and f == 0 and g == 1:
+    print("este valor es tres")
+elif a == 0 and b == 1 and c == 1 and d == 0 and e == 0 and f == 1 and g == 1:
+    print("este valor es cuatro")
+elif a == 1 and b == 0 and c == 1 and d == 1 and e == 0 and f == 1 and g == 1:
+    print("este valor es cinco")
+elif a == 1 and b == 0 and c == 1 and d == 1 and e == 1 and f == 1 and g == 1:
+    print("este valor es seis")
+elif a == 1 and b == 1 and c == 1 and d == 0 and e == 0 and f == 1 and g == 0:
+    print("este valor es siete")
+elif a == 1 and b == 1 and c == 1 and d == 1 and e == 1 and f == 1 and g == 1:
+    print("este valor es 8")
+elif a == 1 and b == 1 and c == 1 and d == 0 and e == 0 and f == 1 and g == 1:
+    print("este valor es 9")
+else:
+    print("el numero no existe")
